e7aa5b6863f2f78454fc7b4c1fc6d30a3c38faf1
yuhave2bmad <dimcoyk@gmail.com>
2020-10-17 21:33:29 +0300

    add edition cleaner and clone parser

--- src/resources/ps4Creds.js
+++ src/resources/ps4Creds.js
@@ -1,9 +1,18 @@
 const PS4_API_PATH = 'https://store.playstation.com/valkyrie-api/en/US/999/container/STORE-MSF77008-ALLGAMES';
 const PS4_NEWEST_GAMES_API_PATH = 'https://store.playstation.com/valkyrie-api/en/US/999/container/STORE-MSF77008-NEWGAMESGRID';
 const WRONG_GAME_NAMES = ['Pre-Order', 'Downloadable Game', 'Game', 'Full Game', 'Bundle', 'Full Game and Add-On Content', 'DCL - The Game', 'Add-On Content', "Play First Trial"];
+const EDITIONS = ["Standard Edition", "Digital Deluxe Edition", "Deluxe Edition", "Ultimate Edition",
+    "Gold Edition", "Standard Edition", "COMPLETE EDITION", "Console Edition", "Anniversary Edition", "Enhanced Edition", "Edition", "Pre-Order"]
+const NO_SPECIAL_CHARACTERS_REGEX = /[^a-zA-Z 0-9\s:]/g;
+const NO_EXTRA_SPACES_REGEX = /\s+/g;
+const LAST_COLON_REGEX = /:$/
 
 module.exports = Object.freeze({
     PS4_API_PATH,
     WRONG_GAME_NAMES,
-    PS4_NEWEST_GAMES_API_PATH
+    PS4_NEWEST_GAMES_API_PATH,
+    EDITIONS,
+    NO_SPECIAL_CHARACTERS_REGEX,
+    NO_EXTRA_SPACES_REGEX,
+    LAST_COLON_REGEX
 });
--- src/services/steal.js
+++ src/services/steal.js
@@ -1,7 +1,8 @@
 const axios = require("axios");
 const moment = require("moment")
 const {saveDumpGames} = require('./persistence')
-const {PS4_API_PATH, PS4_NEWEST_GAMES_API_PATH, WRONG_GAME_NAMES} = require('../resources/ps4Creds');
+const {PS4_API_PATH, PS4_NEWEST_GAMES_API_PATH, WRONG_GAME_NAMES, EDITIONS, NO_EXTRA_SPACES_REGEX,
+    LAST_COLON_REGEX, NO_SPECIAL_CHARACTERS_REGEX} = require('../resources/ps4Creds');
 const logger = require('../utils/logger')
 
 const ps4DefaultParams = {
@@ -11,24 +12,60 @@ const ps4DefaultParams = {
 
 const splitArrays = (...arrays) => arrays.reduce((result, currentArray) => [...result, ...currentArray], [])
 
-const parseGame = game => ({
-    ps4Id: game.id,
-    title: game.attributes.name.trim(),
-    description: game.attributes["long-description"],
-    image: game.attributes['thumbnail-url-base'],
-    releaseDate: game.attributes["release-date"],
-    gameContentType: game.attributes["game-content-type"],
-    prices: game.attributes.skus
-        ? Object.keys(game.attributes.skus[0].prices).map(key => {
-            return {userType: key, price: game.attributes.skus[0].prices[key]['actual-price'].display}
-        })
-        : null,
-    psnURL: `https://store.playstation.com/en-us/product/${game.id}`,
-    timestamp: moment()
-    // metadataDump: game.attributes
-})
+const VER_VALUE = "ver."
+
+const parseTitle = (title) => {
+    const edition = EDITIONS.find(item => title.includes(item))
+    return title
+        .split(edition)[0]
+        .split(VER_VALUE)[0]
+        .replace(NO_SPECIAL_CHARACTERS_REGEX, "")
+        .replace(NO_EXTRA_SPACES_REGEX, " ")
+        .trim()
+        .replace(LAST_COLON_REGEX, "");
+}
+
+const parseGame = game => {
+    const gameTitle = game.attributes.name ? game.attributes.name.trim() : null;
+
+    return {
+        ps4Id: game.id,
+        title: gameTitle && parseTitle(gameTitle),
+        originalTitle: gameTitle,
+        description: game.attributes["long-description"],
+        image: game.attributes['thumbnail-url-base'],
+        releaseDate: game.attributes["release-date"],
+        gameContentType: game.attributes["game-content-type"],
+        prices: game.attributes.skus
+            ? Object.keys(game.attributes.skus[0].prices).map(key => {
+                return {userType: key, price: game.attributes.skus[0].prices[key]['actual-price'].display}
+            })
+            : null,
+        psnURL: `https://store.playstation.com/en-us/product/${game.id}`,
+        timestamp: +moment(),
+        clones: [],
+        metadataDump: game.attributes
+    }
+}
 
 const isGameTitleValid = title => !(title === "Game" || WRONG_GAME_NAMES.includes(title));
+
+const handlePureGames = (gameArray, gameToAdd) => {
+    let isOriginal = true;
+
+    for (const game of gameArray) {
+        if (game.ps4Id === gameToAdd.ps4Id || (gameToAdd.title && game.title && (gameToAdd.title === game.title))) {
+            isOriginal = false;
+            game.clones ? game.clones.push(gameToAdd) : game.clones = [gameToAdd]
+        }
+    }
+
+    if (isOriginal) {
+        gameArray.push(gameToAdd)
+    }
+
+    return gameArray;
+}
 
 const handlePs4Request = async (buckets, apiPath) => {
     logger.debug(`gameBucketsNumber length: ${buckets.length}`)
@@ -49,8 +86,8 @@ const handlePs4Request = async (buckets, apiPath) => {
 
     const [pureGames, dumpGames] = gamesArray
         .reduce((resultArrays, game) => (isGameTitleValid(game.attributes.name) && !game.attributes.parent)
-        ? [[...resultArrays[0], parseGame(game)], resultArrays[1]]
-        : [resultArrays[0], [...resultArrays[1], parseGame(game)]], [[], []]);
+            ? [handlePureGames(resultArrays[0], parseGame(game)), resultArrays[1]]
+            : [resultArrays[0], [...resultArrays[1], parseGame(game)]], [[], []]);
 
     logger.debug(`Total games: ${gamesArray.length}`)
     logger.debug(`Pure games: ${pureGames.length}`)
